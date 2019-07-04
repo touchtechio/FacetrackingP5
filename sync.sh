4379b0c1739bfd28c32541d396720707e81f1ffa
Matt Pinner <mpinner@gmail.com>
2019-07-04 11:20:20 -0700

    added sync script with env var

--- sync.sh
+++ sync.sh
@@ -7,11 +7,11 @@ then
       echo "\$PEM_LOCATION is empty. please set with your key like so 'export PEM_LOCATION=~/Documents/yourkey.pem"
 else
       echo "\$PEM_LOCATION is set to $PEM_LOCATION. syncing..."
+	  
+	  #copy files to amazon
 	  rsync -azvh --progress  -e "ssh -i $PEM_LOCATION" * ubuntu@adellematt.com:FacetrackingP5
+	  
+	  #copy files to public html dir
+	  ssh -i ~/matt-intel-aws.pem ubuntu@adellematt.com  "sudo cp -r ~/FacetrackingP5/Raspy_ridge_2019_04_16_22_44_44/* /var/www/html/"
 fi
  
-#git pull main master
-#rsync -azvh --progress  -e "ssh -i $PEM_LOCATION" * ubuntu@adellematt.com:FacetrackingP5
-#rsync -azvh --progress  -e "ssh -i ~/matt-intel-aws.pem" * ubuntu@54.200.109.247:FacetrackingP5
-
-#scp -r  -i "~/matt-intel-aws.pem" * ubuntu@54.191.170.5:FacetrackingP5
